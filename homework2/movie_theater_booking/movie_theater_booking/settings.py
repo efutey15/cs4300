d561bd94ef45f3a4589b7b1fd21abb0ba35cac30
efutey15 <ejf.colorado@gmail.com>
2026-03-04 15:43:40 +0000

    Disabled debug and added admin creation to build.sh

--- homework2/movie_theater_booking/movie_theater_booking/settings.py
+++ homework2/movie_theater_booking/movie_theater_booking/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -24,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-i@$m@k#p5ixbckbhqtqcx&u9#b10e21v)xc$e-n_r5h@4*wwa8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
